a448c89cdd1075b5214305ada8d2793555bea2d1
sichotmeturgamot <idishshel@gmail.com>
2026-07-03 14:00:52 +0300

    הסתרת שיחה: שבת פרשת נח - בסעודת היום

--- data.js
+++ data.js
@@ -46,7 +46,8 @@ const YEARS = [
         "pages": 1,
         "count": 1,
         "sortDate": "02-01",
-        "hebDate": "א׳ מרחשון"
+        "hebDate": "א׳ מרחשון",
+        "hidden": true
       }
     ]
   }
