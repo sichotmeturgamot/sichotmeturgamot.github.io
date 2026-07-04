c20c27fa5a27dc48bced56405394cd5696c6232a
sichotmeturgamot <idishshel@gmail.com>
2026-07-05 01:23:55 +0300

    Add Google Analytics tracking to public pages

---
+++ analytics.js
@@ -0,0 +1,13 @@
+// Google Analytics (GA4) — ספר השיחות
+(function () {
+  var id = 'G-EY7JC5N8BM';
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + id;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', id);
+})();
