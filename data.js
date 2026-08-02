bcb20a7052afd3cd6d060fe134e9e2b7a9fe3332
sichotmeturgamot <idishshel@gmail.com>
2026-08-02 22:41:28 +0300

    הוספת שיחה: יום א' דחג הסוכות תש"ו

--- data.js
+++ data.js
@@ -56,6 +56,16 @@ const YEARS = [
         "sortDate": "01-14",
         "hebDate": "י״ד תשרי",
         "addedAt": "2026-08-02"
+      },
+      {
+        "title": "יום א' דחג הסוכות תש\"ו",
+        "subtitle": "",
+        "file": "pdfs/Yom_Alef_Sukkos_5706.pdf",
+        "pages": 1,
+        "count": 1,
+        "sortDate": "01-15",
+        "hebDate": "ט״ו תשרי",
+        "addedAt": "2026-08-02"
       }
     ]
   },
