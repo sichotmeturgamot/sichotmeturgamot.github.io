f3a67c4752ac50c470db15c08a7b2e40900c024b
sichotmeturgamot <idishshel@gmail.com>
2026-08-02 23:27:44 +0300

    הוספת שיחה: יום א' דחג השבועות תש"ו

--- data.js
+++ data.js
@@ -266,6 +266,16 @@ const YEARS = [
         "sortDate": "10-05",
         "hebDate": "ה׳ סיון",
         "addedAt": "2026-08-02"
+      },
+      {
+        "title": "יום א' דחג השבועות תש\"ו",
+        "subtitle": "",
+        "file": "pdfs/24_Yom_Alef_Shavuos_5706.pdf",
+        "pages": 1,
+        "count": 1,
+        "sortDate": "10-06",
+        "hebDate": "ו׳ סיון",
+        "addedAt": "2026-08-02"
       }
     ]
   },
