1f08f81bbefbecab976785c02ac3c09d5acb0bf8
sichotmeturgamot <idishshel@gmail.com>
2026-08-24 13:54:15 +0300

    הצגת שיחה: כ"ט אלול תש"ז, ערב ראש השנה תש"ח

--- data.js
+++ data.js
@@ -753,8 +753,7 @@ const YEARS = [
         "sortDate": "01-01",
         "hebDate": "א׳ תשרי",
         "addedAt": "2026-08-13",
-        "hasPage": true,
-        "hidden": true
+        "hasPage": true
       }
     ]
   }
