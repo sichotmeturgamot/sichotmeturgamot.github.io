b69da988e6fa0407039925c96b762611b53631ab
sichotmeturgamot <idishshel@gmail.com>
2026-09-11 15:06:34 +0300

    עריכת שנה: תש"ח

--- data.js
+++ data.js
@@ -876,6 +876,8 @@ const YEARS = [
         "addedAt": "2026-09-08",
         "hasPage": true
       }
-    ]
+    ],
+    "fullFile": "pdfs/Tishrey_5708.pdf",
+    "fullTitle": "שיחות תשרי תש\"ח - עם מ\"מ ומפתחות"
   }
 ];
